16e7fcfc808094042e07abbaa790f2cf464e2781
Hackfront <hackfront@protonmail.ch>
2023-07-17 09:56:09 +0200

    Aggiunte varie funzioni XML a lib/xml.php

---
+++ www/lib/xml.php
@@ -0,0 +1,57 @@
+<?php
+require_once('../config.php');
+
+function load_xml($table) {
+  $xmlFile = RC_ROOT . '/data/' . $table . '.xml';
+
+  $xmlString = '';
+  foreach(file($xmlFile) as $line) {
+    $xmlString .= trim($line);
+  }
+  $doc = new DOMDocument();
+  $doc->loadXML($xmlString);
+
+  return $doc;
+}
+
+function save_xml($doc, $table) {
+  $xmlFile = RC_ROOT . '/data/' . $table . '.xml';
+
+  $doc->save($xmlFile);
+}
+
+
+function domlist_to_array($domlist) {
+  $arr = [];
+  for ($i = 0; $i < $domlist->length; $i++) {
+    $arr[$i] = $domlist->item($i);
+  }
+  return $arr;
+}
+
+
+function sort_by_element_dec($elements, $key) {
+  usort($elements, function($aElement, $bElement) use ($key) {
+    $aValue = $aElement->getElementsByTagName($key)[0]->textContent;
+    $bValue = $bElement->getElementsByTagName($key)[0]->textContent;
+
+    $aValue = floatval($aValue);
+    $bValue = floatval($bValue);
+
+    return $aValue <=> $bValue;
+  });
+
+  return $elements;
+}
+
+function sort_by_element_txt($elements, $key) {
+  usort($elements, function($aElement, $bElement) use ($key) {
+    $aValue = $aElement->getElementsByTagName($key)[0]->textContent;
+    $bValue = $bElement->getElementsByTagName($key)[0]->textContent;
+
+    return strnatcmp($aValue, $bValue);
+  });
+
+  return $elements;
+}
+?>
