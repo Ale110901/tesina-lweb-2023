21a05a43489bc40e38084c1acef81d95d00cbd1e
alessandro <alececchetto2001@gmail.com>
2023-07-26 17:47:29 +0200

    modificato header

--- www/lib/header.php
+++ www/lib/header.php
@@ -1,12 +1,13 @@
 <div id="header">
   <div id="icone">
-    <a href="#">&#x01F464</a>
-    <a href="#">&#x01F6D2</a>
+    <a href="#" title="profilo">&#x01F464</a>   
+    <a href="#" title="carrello">&#x01F6D2</a>
+    <span id="numero-item-carrello">0</span>
   </div>
   <h1 id="titolo"><a href="index.php">R&amp;C store</a></h1>
   <ul id="menu">
     <li><a href="index.php">Homepage</a></li>
-    <li><a href="corsi.php">Corsi</a></li>
+    <li><a href="corsi.php">Catalogo</a></li>
     <li><a href="servizi.php">Servizi</a></li>
     <li><a href="shop.php">Shop</a></li>
     <li><a href="info.php">Informazioni</a></li>
