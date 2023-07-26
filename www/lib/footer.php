9f0fca2b9e3323016ff610f5d50af0900258821a
Hackfront <hackfront@protonmail.ch>
2023-07-26 13:18:44 +0200

    Aggiunti header e footer generali

---
+++ www/lib/footer.php
@@ -0,0 +1,20 @@
+<hr id="riga-sezioni" />
+<div id="footer" class="centrato">
+  <div id="sopra" class="py-1em">
+    <div>
+      <h3 class="pb-8">Contatti</h3>
+      <p id="contatti">
+        &#x01F4E7 cecchetto.1941039@studenti.uniroma1.it<br>
+        &#x01F4E7 roccia.1967318@studenti.uniroma1.it<br>
+        &#x01F4DE +39 07731234567
+      </p>
+    </div>
+    <div>
+      <h3 class="pb-8">Dove siamo</h3>
+      &#x01F4CC Via XXIV Maggio, 7, 04100 Latina LT
+    </div>
+  </div>
+  <div id="sotto" class="py-1em">
+    Copyright R&amp;C store 2023
+  </div>
+</div>
---
+++ www/lib/header.php
@@ -0,0 +1,15 @@
+<div id="header">
+  <div id="icone">
+    <a href="#">&#x01F464</a>
+    <a href="#">&#x01F6D2</a>
+  </div>
+  <h1 id="titolo"><a href="index.php">R&amp;C store</a></h1>
+  <ul id="menu">
+    <li><a href="index.php">Homepage</a></li>
+    <li><a href="corsi.php">Corsi</a></li>
+    <li><a href="servizi.php">Servizi</a></li>
+    <li><a href="shop.php">Shop</a></li>
+    <li><a href="info.php">Informazioni</a></li>
+  </ul>
+</div>
+<hr id="riga-sezioni" />
