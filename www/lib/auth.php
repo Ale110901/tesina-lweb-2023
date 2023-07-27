0c128f46efed6b8957cb139cadc26540beda1f70
alessandro <alececchetto2001@gmail.com>
2023-07-27 17:23:19 +0200

    tolti commenti

--- www/lib/auth.php
+++ www/lib/auth.php
@@ -1,26 +1,9 @@
 <?php
 require_once('xml.php');
 
-/* eventualmente da fare con xpath... */
 function access_verification($email, $password){
   $doc = load_xml('utenti');
-
-  /*$root = $doc->documentElement;
-  $utenti = $root->childNodes;
-  $trovato = false;
-  $i = 0;
-  do{
-    $utente = $utenti->item($i);
-    $password_utente = $utente->getElementsByTagName('password')[0]->textContent;
-    $attivo_utente = $utente->getElementsByTagName('attivo')[0]->textContent;
-    $email_utente = $utente->getAttribute('email');
-    if($email_utente == $email && $password_utente == $password && $attivo_utente == true){
-      $trovato = true;
-    }
-    $i++;
-  }while($i < $utenti->length && !$trovato);*/
-  /* DA SISTEMARE */
-
+  
   $xpath = new DOMXPath($doc);
   $xpath->registerNameSpace('ut', 'http://www.lweb.uni/tesina-rcstore/utenti/');
   $query = "/ut:utenti/ut:utente[@email='$email']";
