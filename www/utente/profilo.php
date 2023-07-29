91b57b69b03ea2c5940ea1ec8d7fff27b5e075df
Hackfront <hackfront@protonmail.ch>
2023-07-29 12:06:54 +0200

    Sistemato form profilo

--- www/utente/profilo.php
+++ www/utente/profilo.php
@@ -1,21 +1,35 @@
-<?php 
-  require('../config.php');
-  require(RC_ROOT . '/lib/start.php');
-  require(RC_ROOT . '/lib/auth.php');
+<?php
+  require_once('../config.php');
+  require_once(RC_ROOT . '/lib/start.php');
+  require_once(RC_ROOT . '/lib/auth.php');
+  require_once(RC_ROOT . '/lib/xml.php');
 
   $id_utente = access_verification('mario.rossi@gmail.com', 'password');
-  if($id_utente !== false){
-    $nodo_utente = estrazione_utente($id_utente);
+
+  if ($id_utente !== false) {
+    $doc_utenti = load_xml('utenti');
+    $nodo_utente = estrazione_utente($doc_utenti, $id_utente);
+
+    if (isset($_POST['azione']) && $_POST['azione'] === 'modifica') {
+      $nodo_utente->getElementsByTagName('nome')[0]->textContent = $_POST['nome'];
+      $nodo_utente->getElementsByTagName('cognome')[0]->textContent = $_POST['cognome'];
+      $nodo_utente->getElementsByTagName('telefono')[0]->textContent = $_POST['telefono'];
+      $nodo_utente->getElementsByTagName('indirizzo')[0]->textContent = $_POST['indirizzo'];
+      $nodo_utente->getElementsByTagName('codiceFiscale')[0]->textContent = $_POST['codice_fiscale'];
+
+      save_xml($doc_utenti, 'utenti');
+    }
+
     $nome = $nodo_utente->getElementsByTagName('nome')[0]->textContent;
     $cognome = $nodo_utente->getElementsByTagName('cognome')[0]->textContent;
+    $email = $nodo_utente->getAttribute('email');
     $telefono = $nodo_utente->getElementsByTagName('telefono')[0]->textContent;
     $indirizzo = $nodo_utente->getElementsByTagName('indirizzo')[0]->textContent;
-    $codiceFiscale = $nodo_utente->getElementsByTagName('codiceFiscale')[0]->textContent;
+    $codice_fiscale = $nodo_utente->getElementsByTagName('codiceFiscale')[0]->textContent;
     $credito = $nodo_utente->getElementsByTagName('credito')[0]->textContent;
     $reputazione = $nodo_utente->getElementsByTagName('reputazione')[0]->textContent;
   }
 ?>
-
 <?xml version="1.0" encoding="UTF-8" ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="it" lang="it">
@@ -24,95 +38,95 @@
 
   <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Rampart+One&amp;display=swap" />
 
-  <link rel="stylesheet" type="text/css" href="../res/css/common.css" />
-  <link rel="stylesheet" type="text/css" href="../res/css/header.css" />
-  <link rel="stylesheet" type="text/css" href="../res/css/footer.css" />
-  <link rel="stylesheet" type="text/css" href="../res/css/profilo.css" />
+  <link rel="stylesheet" type="text/css" href="<?php echo(RC_SUBDIR); ?>/res/css/common.css" />
+  <link rel="stylesheet" type="text/css" href="<?php echo(RC_SUBDIR); ?>/res/css/header.css" />
+  <link rel="stylesheet" type="text/css" href="<?php echo(RC_SUBDIR); ?>/res/css/footer.css" />
+  <link rel="stylesheet" type="text/css" href="<?php echo(RC_SUBDIR); ?>/res/css/profilo.css" />
 </head>
 
 <body>
 
   <script>
-    function visibilita(){
-      var element1 = document.getElementById("info-profilo");
-      var element2 = document.getElementById("modifiche-profilo2");
-      var element3 = document.getElementById("modifiche-profilo3");
-      var element4 = document.getElementById("modifiche-profilo4");
-      var element5 = document.getElementById("modifiche-profilo5");
-      var element6 = document.getElementById("modifiche-profilo6");
-      element1.classList.toggle("mostra-modifiche-profilo");
-      element2.classList.toggle("mostra-modifiche-profilo");
-      element3.classList.toggle("mostra-modifiche-profilo");
-      element4.classList.toggle("mostra-modifiche-profilo");
-      element5.classList.toggle("mostra-modifiche-profilo");
-      element6.classList.toggle("mostra-modifiche-profilo");
+    function visibilita() {
+      var elemInfo = document.getElementsByClassName('info-mod');
+      for (var i = 0; i < elemInfo.length; i++) {
+        elemInfo[i].classList.toggle("nascosto");
+      }
+
+      var elemInfo = document.getElementsByClassName('input-profilo');
+      for (var i = 0; i < elemInfo.length; i++) {
+        elemInfo[i].classList.toggle("nascosto");
+      }
+
+      var elemInfo = document.getElementsByClassName('button-modifica-profilo');
+      for (var i = 0; i < elemInfo.length; i++) {
+        elemInfo[i].classList.toggle("nascosto");
+      }
     }
   </script>
 
-  <?php require(RC_ROOT . '/lib/header.php'); ?> 
+  <?php require(RC_ROOT . '/lib/header.php'); ?>
   <div id="card-esterna-profilo" class="py-1em">
     <h2>Profilo</h2>
-      <form id="form-profilo" action="profilo.php" method="POST">
+      <form id="form-profilo" action="<?php echo(RC_SUBDIR); ?>/utente/profilo.php" method="POST">
         <table id="table-info-profilo" class="py-1em">
           <tr>
-            <td id="specifica-campo"><b>Nome:</b></td>
-            <td> 
-              <span id="info-profilo"> <?php echo $nome; ?> </span> 
-              <input type="text" class="input-profilo" id="modifiche-profilo1" name="reset_nome">
-            </td>
-          </tr>
-          <tr>
-            <td id="specifica-campo"><b>Cognome:</b></td>
-            <td> 
-              <span id="info-profilo"> <?php echo $cognome; ?> </span>
-              <input type="text" class="input-profilo" id="modifiche-profilo2" name="reset_cognome">
-            </td>
-          </tr>
-          <tr>
-            <td id="specifica-campo"><b>Email:</b></td>
+            <td class="specifica-campo"><b>Nome:</b></td>
             <td>
-              <span id="info-profilo"> <?php  ?> </span>
-              <input type="text" class="input-profilo" id="modifiche-profilo3" name="reset_email">
+              <span class="info-mod"><?php echo ($nome); ?></span>
+              <input type="text" class="input-profilo nascosto" name="nome" value="<?php echo ($nome); ?>" />
             </td>
           </tr>
           <tr>
-            <td id="specifica-campo"><b>Telefono:</b></td>
+            <td class="specifica-campo"><b>Cognome:</b></td>
             <td>
-              <span id="info-profilo"> <?php echo $telefono; ?> </span>
-              <input type="text" class="input-profilo" id="modifiche-profilo4" name="reset_telefono">
+              <span class="info-mod"><?php echo ($cognome); ?></span>
+              <input type="text" class="input-profilo nascosto" name="cognome" value="<?php echo ($cognome); ?>" />
             </td>
           </tr>
           <tr>
-            <td id="specifica-campo"><b>Indirizzo:</b></td>
+            <td class="specifica-campo"><b>Email:</b></td>
             <td>
-              <span id="info-profilo"> <?php echo $indirizzo; ?> </span>
-              <input type="text" class="input-profilo" id="modifiche-profilo5" name="reset_indirizzo">
+              <span class="info-profilo"><?php echo ($email); ?></span>
             </td>
           </tr>
           <tr>
-            <td id="specifica-campo"><b>Codice fiscale:</b></td>
+            <td class="specifica-campo"><b>Telefono:</b></td>
             <td>
-              <span id="info-profilo"> <?php echo $codiceFiscale; ?> </span>
-              <input type="text" class="input-profilo" id="modifiche-profilo6" name="reset_cf">
+              <span class="info-mod"><?php echo ($telefono); ?></span>
+              <input type="text" class="input-profilo nascosto" name="telefono" value="<?php echo ($telefono); ?>" />
             </td>
           </tr>
           <tr>
-            <td id="specifica-campo"><b>Credito:</b></td>
+            <td class="specifica-campo"><b>Indirizzo:</b></td>
             <td>
-              <span id="info-profilo"> <?php echo $credito; ?> &euro;</span>
-              <!--<input type="text" id="input-profilo" name="reset_indirizzo">-->
+              <span class="info-mod"><?php echo ($indirizzo); ?></span>
+              <input type="text" class="input-profilo nascosto" name="indirizzo" value="<?php echo ($indirizzo); ?>" />
             </td>
           </tr>
           <tr>
-            <td id="specifica-campo"><b>Reputazione:</b></td>
+            <td class="specifica-campo"><b>Codice fiscale:</b></td>
             <td>
-              <?php echo $reputazione; ?>
+              <span class="info-mod"><?php echo ($codice_fiscale); ?></span>
+              <input type="text" class="input-profilo nascosto" name="codice_fiscale" value="<?php echo ($codice_fiscale); ?>" />
+            </td>
+          </tr>
+          <tr>
+            <td class="specifica-campo"><b>Credito:</b></td>
+            <td>
+              <span class="info-profilo"><?php echo ($credito); ?> &euro;</span>
+            </td>
+          </tr>
+          <tr>
+            <td class="specifica-campo"><b>Reputazione:</b></td>
+            <td>
+              <span class="info-profilo"><?php echo ($reputazione); ?> punti</span>
             </td>
           </tr>
         </table>
-        <button type="submit" class="button-conferma-profilo" id="conferma-modifiche" name="azione" value="conferma_modifiche_profilo" title="conferma modifiche">Conferma modifiche</button>
+        <button type="submit" class="button-modifica-profilo nascosto" name="azione" value="modifica" title="conferma modifiche">Conferma modifiche</button>
       </form>
-      <button type="submit" class="button-modifica-profilo mt-8" id="modifica" name="azione" value="cambia_profilo" title="modifica profilo" onclick="visibilita()">Modifica profilo</button>
+      <button class="button-modifica-profilo mt-8" title="modifica profilo" onclick="visibilita()">Modifica profilo</button>
   </div>
 
   <?php require(RC_ROOT . '/lib/footer.php'); ?>
