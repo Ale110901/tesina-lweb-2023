6d9a318bb55d483eb3bb25b8e73ef1bc42610acd
Hackfront <hackfront@protonmail.ch>
2023-07-29 13:26:33 +0200

    Sistemati percorsi header

--- www/lib/header.php
+++ www/lib/header.php
@@ -1,7 +1,7 @@
 <div id="header">
   <div id="icone">
-<?php if ($sessione) { ?>
-    <a href="<?php echo(RC_SUBDIR); ?>/profilo.php" title="Profilo">&#x01F464</a>
+<?php if ($loggato) { ?>
+    <a href="<?php echo(RC_SUBDIR); ?>/utente/profilo.php" title="Profilo">&#x01F464</a>
 <?php } else { ?>
     <a href="<?php echo(RC_SUBDIR); ?>/registrazione.php" title="profilo">&#x01F464</a>
 <?php }?>
@@ -11,7 +11,7 @@
   <h1 id="titolo"><a href="<?php echo(RC_SUBDIR); ?>/index.php">R&amp;C store</a></h1>
   <ul id="menu">
     <li><a href="<?php echo(RC_SUBDIR); ?>/index.php">Homepage</a></li>
-    <li><a href="<?php echo(RC_SUBDIR); ?>/corsi.php">Catalogo</a></li>
+    <li><a href="<?php echo(RC_SUBDIR); ?>/catalogo.php">Catalogo</a></li>
     <li><a href="<?php echo(RC_SUBDIR); ?>/servizi.php">Servizi</a></li>
     <li><a href="<?php echo(RC_SUBDIR); ?>/shop.php">Shop</a></li>
     <li><a href="<?php echo(RC_SUBDIR); ?>/info.php">Informazioni</a></li>
