4d8d80434b29ac76e935ac1a9ce56df119135759
Hackfront <hackfront@protonmail.ch>
2023-07-31 00:57:46 +0200

    Rimossa prova accesso.php

--- www/prove/accesso.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once('../lib/auth.php');
-
-header('Content-Type: text/plain');
-
-$email = "mario.rossi@gmail.com";
-$password = "password";
-access_verification($email, $password);
-?>
