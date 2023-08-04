3ae6326f6ab928af628371205668f3eddcb7785e
Hackfront <hackfront@protonmail.ch>
2023-08-04 13:50:44 +0200

    Modificato stile pagina prodotto

--- www/prodotto.php
+++ www/prodotto.php
@@ -8,6 +8,7 @@ $perm_admin = true;
 
 require_once(RC_ROOT . '/lib/start.php');
 require_once(RC_ROOT . '/lib/xml.php');
+require_once(RC_ROOT . '/lib/offerte.php');
 
 $id_valido = isset($_GET['id']) && !is_nan($_GET['id']);
 if ($id_valido) {
@@ -23,9 +24,13 @@ if ($id_valido) {
     $nome = $prodotto->getElementsByTagName('nome')[0]->textContent;
     $marca = $prodotto->getElementsByTagName('marca')[0]->textContent;
     $descrizione = $prodotto->getElementsByTagName('descrizione')[0]->textContent;
-    $costo = $prodotto->getElementsByTagName('costo')[0]->textContent;
+    $costo_orig = $prodotto->getElementsByTagName('costo')[0]->textContent;
     $categoria = $prodotto->getElementsByTagName('categoria')[0]->textContent;
     $quantita = $prodotto->getElementsByTagName('quantita')[0]->textContent;
+
+    $sconto = calcola_sconto($id_prodotto);
+    $crediti = calcola_crediti($id_prodotto);
+    $costo_finale = round($costo_orig * (1 - $sconto), 2);
   }
 }
 ?>
@@ -34,7 +39,7 @@ if ($id_valido) {
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="it" lang="it">
 <head>
-  <title>Prodotto &ndash; R&amp;C store</title>
+  <title><?php echo($nome); ?> &ndash; R&amp;C store</title>
 
   <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Rampart+One&amp;display=swap" />
 
@@ -51,11 +56,55 @@ if ($id_valido) {
 <?php if (!$id_valido) { ?>
     <p>Prodotto non trovato.</p>
 <?php } else { ?>
-    <img id="img-prodotto" src="<?php echo(RC_SUBDIR); ?>/res/img/prodotti/<?php echo($id_prodotto); ?>.png" alt="Immagine prodotto <?php echo($id_prodotto); ?>" ></img>
-    <div id="descrizione">
-      <p><i><?php echo($marca); ?></i></p>
-      <p><b><?php echo($nome); ?></b></p>
-      <p><?php echo($descrizione); ?></p>
+    <div id="pag-prodotto">
+      <div id="prod-top">
+        <div id="prod-top-sx">
+          <img src="<?php echo(RC_SUBDIR); ?>/res/img/prodotti/<?php echo($id_prodotto); ?>.png" alt="Immagine prodotto <?php echo($id_prodotto); ?>" ></img>
+        </div>
+        <div id="prod-top-mid">
+          <p id="marca"><?php echo($marca); ?></p>
+          <p id="nome"><?php echo($nome); ?></p>
+          <p id="descrizione"><?php echo($descrizione); ?></p>
+        </div>
+        <div id="prod-top-dx">
+          <div id="prod-top-dx-info">
+            <div id="col-1">
+<?php if ($sconto > 0.00) { ?>
+              <p>&#x1F4B2; <?php echo($sconto * 100); ?>% di sconto</p>
+<?php } ?>
+<?php if ($crediti > 0) { ?>
+              <p>&#x1F4B2; +<?php echo($crediti); ?> crediti</p>
+<?php } ?>
+            </div>
+            <div id="col-2">
+<?php if ($costo_orig - $costo_finale >= 0.01) { ?>
+              <span class="barrato"><?php echo($costo_orig); ?> &euro;</span>
+<?php } ?>
+              <span><?php echo($costo_finale); ?> &euro;</span>
+            </div>
+          </div>
+          <form id="prod-top-dx-action" action="<?php echo(RC_SUBDIR); ?>/cliente/carrello.php" method="post">
+            <input type="hidden" name="id_prodotto" value="<?php echo($id_prodotto); ?>" />
+            <div id="input-qta">
+              <input type="number" name="quantita" class="input-box" value="0" min="0" step="1" size="3" max="99" />
+            </div>
+            <div id="btn-aggiungi">
+              <button type="submit" name="azione" value="aggiungi" class="button ml-8">Aggiungi al carrello</button>
+            </div>
+          </form>
+        </div>
+      </div>
+      <hr />
+      <div>
+        <h3>Recensioni</h3>
+        <p>E' buono.</p>
+      </div>
+      <hr />
+      <div>
+        <h3>Domande e risposte</h3>
+        <h4>Ciao?</h4>
+        <p>Ciao.</p>
+      </div>
     </div>
 <?php } ?>
   </div>
