d6c2755584ebf7f49aebed9e0a93f17b886d9659
Hackfront <hackfront@protonmail.ch>
2023-08-04 02:20:07 +0200

    Aggiunta checkbox e label sui prodotti out-of-stock

--- www/catalogo.php
+++ www/catalogo.php
@@ -14,8 +14,11 @@ $doc_prod = load_xml('prodotti');
 $prodotti = $doc_prod->documentElement->childNodes;
 $prodotti = domlist_to_array($prodotti);
 
-$ord_desc = $_GET['desc'] === 'on';
-switch ($_GET['ordina']) {
+$ord_type = isset($_GET['ordina']) ? $_GET['ordina'] : 'nome';
+$ord_desc = isset($_GET['desc']) && $_GET['desc'] === 'on';
+$solo_disp = isset($_GET['solo_disp']) && $_GET['solo_disp'] === 'on';
+
+switch ($ord_type) {
   case 'nome':
     $prodotti = sort_by_element_txt($prodotti, 'nome', $ord_desc);
     break;
@@ -49,32 +52,38 @@ switch ($_GET['ordina']) {
   <div id="contenuto" class="centrato">
     <h2 class="pb-32">CATALOGO</h2>
     <form action="<?php echo(RC_SUBDIR); ?>/catalogo.php" method="get">
-      <label for="sort">Ordinamento:</label>
+      <label for="ordina">Ordinamento:</label>
 
       <select name="ordina">
-        <option value="nome">Nome</option>
-        <option value="categoria">Categoria</option>
-        <option value="marca">Marca</option>
-        <option value="prezzo">Prezzo</option>
+        <option value="nome" <?php if ($ord_type === 'nome') echo ('selected="true"'); ?>>Nome</option>
+        <option value="categoria" <?php if ($ord_type === 'categoria') echo ('selected="true"'); ?>>Categoria</option>
+        <option value="marca" <?php if ($ord_type === 'marca') echo ('selected="true"'); ?>>Marca</option>
+        <option value="prezzo" <?php if ($ord_type === 'prezzo') echo ('selected="true"'); ?>>Prezzo</option>
       </select>
 
-      <input type="checkbox" name="desc">Ordine contrario</input>
+      <input type="checkbox" name="desc" <?php if ($ord_desc) echo ('checked="true"'); ?>>Ordine contrario</input>
+      <input type="checkbox" name="solo_disp" <?php if ($solo_disp) echo ('checked="true"'); ?>>Solo in stock</input>
 
       <button type="submit">Invia</button>
     </form>
     <div id="catalogo">
 <?php
   foreach ($prodotti as $prodotto) {
+    $p_quantita = $prodotto->getElementsByTagName('quantita')[0]->textContent;
+    if ($p_quantita === '0' && $solo_disp) {
+      continue;
+    }
+
     $p_id = $prodotto->getAttribute('id');
     $p_costo = $prodotto->getElementsByTagName('costo')[0]->textContent;
     $p_nome = $prodotto->getElementsByTagName('nome')[0]->textContent;
     $p_marca = $prodotto->getElementsByTagName('marca')[0]->textContent;
     $p_categoria = $prodotto->getElementsByTagName('categoria')[0]->textContent;
-    $p_quantita = $prodotto->getElementsByTagName('quantita')[0]->textContent;
 ?>
-      <div>
-        <a href="<?php echo(RC_SUBDIR); ?>/prodotto.php?id=<?php echo($p_id); ?>" class="">
-          <img id="img-prodotto" src="<?php echo(RC_SUBDIR); ?>/res/img/prodotti/<?php echo($p_id); ?>.png" alt="Immagine prodotto <?php echo($p_id); ?>" ></img>
+      <div class="card-prodotto <?php if ($p_quantita === '0') echo('out-of-stock'); ?>">
+        <a href="<?php echo(RC_SUBDIR); ?>/prodotto.php?id=<?php echo($p_id); ?>">
+          <img src="<?php echo(RC_SUBDIR); ?>/res/img/prodotti/<?php echo($p_id); ?>.png" alt="Immagine prodotto <?php echo($p_id); ?>"></img>
+          <div class="overlay">terminato</div>
           <p><?php echo(ottieni_categoria($p_categoria)); ?></p>
           <p><?php echo($p_marca); ?></p>
           <p><?php echo($p_nome); ?></p>
