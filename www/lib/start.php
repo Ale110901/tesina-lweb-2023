f2c957c119cfc039da25f5684246d092fdc54b99
Hackfront <hackfront@protonmail.ch>
2023-08-04 14:01:03 +0200

    Aggiunta gestione aggiunta elemento al carrello

--- www/lib/start.php
+++ www/lib/start.php
@@ -10,6 +10,21 @@ if (RC_ROOT === null) {
 
 session_start();
 
+// Caso particolare
+/*
+ * La pagina del carrello riceve un post quando si aggiunge un elemento.
+ * Serve memorizzare i dati del post anche quando l'utente non e' loggato per
+ * permettere l'aggiunta dell'elemento dopo il login.
+ * Quindi questa operazione va in questo file, perche' va eseguita dopo
+ * l'avvio della sessione ma prima del controllo dei permessi.
+ */
+if ($_SERVER['PHP_SELF'] === RC_SUBDIR . '/cliente/carrello.php') {
+  if (isset($_POST['azione']) && $_POST['azione'] === 'aggiungi') {
+    $_SESSION['agg_carr_id_prod'] = $_POST['id_prodotto'];
+    $_SESSION['agg_carr_qta'] = $_POST['quantita'];
+  }
+}
+
 $loggato = isset($_SESSION['id_utente']) && !is_nan($_SESSION['id_utente']);
 
 if (!$perm_visitatore) {
