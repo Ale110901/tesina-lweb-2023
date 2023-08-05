ad4aa8332a9b53bfd04803315816de94f7541cbb
Hackfront <hackfront@protonmail.ch>
2023-08-05 15:49:07 +0200

    Terminata funzione calcola_rating_medio()

--- www/lib/rating.php
+++ www/lib/rating.php
@@ -1,8 +1,28 @@
 <?php
 function calcola_rating_medio($ratings) {
+  if ($ratings->length === 0) {
+    return [
+      'supporto' => 0.00,
+      'utilita' =>  0.00
+    ];
+  }
+
+  $acc_supp = 0.00;
+  $acc_util = 0.00;
+
+  for ($i = 0; $i < $ratings->length; $i++) {
+    $rating = $ratings[$i];
+
+    $rat_supp = $rating->getElementsByTagName('supporto')[0]->textContent;
+    $rat_util = $rating->getElementsByTagName('utilita')[0]->textContent;
+
+    $acc_supp += $rat_supp;
+    $acc_util += $rat_util;
+  }
+
   return [
-    'supporto' => 1.7,
-    'utilita' => 2.3
+    'supporto' => $acc_supp / $ratings->length,
+    'utilita' =>  $acc_util / $ratings->length
   ];
 }
 ?>
