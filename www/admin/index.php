2de0cd167be2f571dad6dc71e878256f2173659f
Hackfront <hackfront@protonmail.ch>
2023-08-05 11:38:48 +0200

    Aggiunta base dashboard admin e gestore

--- www/admin/index.php
+++ www/admin/index.php
@@ -1,10 +1,30 @@
+<?php
+require_once('../config.php');
+
+$perm_visitatore = false;
+$perm_cliente = false;
+$perm_gestore = false;
+$perm_admin = true;
+
+require_once(RC_ROOT . '/lib/start.php');
+?>
 <?xml version="1.0" encoding="UTF-8" ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="it" lang="it">
 <head>
+  <title>Dashboard admin &ndash; R&amp;C store</title>
 
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Rampart+One&amp;display=swap" />
+
+  <link rel="stylesheet" type="text/css" href="<?php echo(RC_SUBDIR); ?>/res/css/common.css" />
+  <link rel="stylesheet" type="text/css" href="<?php echo(RC_SUBDIR); ?>/res/css/header.css" />
+  <link rel="stylesheet" type="text/css" href="<?php echo(RC_SUBDIR); ?>/res/css/footer.css" />
 </head>
 <body>
-
+  <?php require(RC_ROOT . '/lib/header.php'); ?>
+  <div id="contenuto">
+    <h2>DASHBOARD ADMIN</h2>
+  </div>
+  <?php require(RC_ROOT . '/lib/footer.php'); ?>
 </body>
 </html>
