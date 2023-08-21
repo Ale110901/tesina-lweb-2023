d76303b3069c5022d37c458acdda48dd346eeb62
Hackfront <hackfront@protonmail.ch>
2023-08-21 16:25:23 +0200

    Rimosso permesso modifica offerte admin

--- www/gestore/offerta.php
+++ www/gestore/offerta.php
@@ -4,7 +4,7 @@ require_once('../config.php');
 $perm_visitatore = false;
 $perm_cliente = false;
 $perm_gestore = true;
-$perm_admin = true;
+$perm_admin = false;
 
 require_once(RC_ROOT . '/lib/start.php');
 require_once(RC_ROOT . '/lib/categorie.php');
